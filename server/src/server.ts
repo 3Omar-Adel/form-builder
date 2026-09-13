4d599237a7a1a45ff5bdca90ba840f8522355920
Omar Adel <oadel425@gmail.com>
2026-09-13 13:08:42 +0300

    g

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,3 +5,5 @@ const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// DATABASE_URL="postgresql://neondb_owner:npg_LreV9Zb3hcTx@ep-fragrant-block-ayxtq2ef-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
