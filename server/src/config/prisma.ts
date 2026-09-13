9524a0b9e12fe4e4d707c10cebb990fa221a3b01
Omar Adel <oadel425@gmail.com>
2026-09-13 13:26:11 +0300

    y

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,12 +1,20 @@
 import "dotenv/config";
 
 import { PrismaPg } from "@prisma/adapter-pg";
+
 import { PrismaClient } from "../generated/prisma/client";
 
+console.log(
+    "DATABASE HOST:",
+    process.env.DATABASE_URL
+        ?.split("@")[1]
+        ?.split("/")[0]
+);
+
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export const prisma = new PrismaClient({
-  adapter,
+    adapter,
 });
